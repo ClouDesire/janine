1500a342df288dc26938e64bbe34bc3121433d53
Matteo Giordano <matteo.giordano@cloudesire.com>
2015-12-14 15:01:57 +0100

    Add basic test

--- server/src/main/java/com/liberologico/invoice_api/controllers/InvoiceController.java
+++ server/src/main/java/com/liberologico/invoice_api/controllers/InvoiceController.java
@@ -1,23 +1,19 @@
 package com.liberologico.invoice_api.controllers;
 
 import com.liberologico.invoice_api.entities.Invoice;
-import com.liberologico.invoice_api.entities.Line;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.stream.Collectors;
 
 @RestController
 public class InvoiceController
 {
     @RequestMapping( value = "/", method = RequestMethod.POST, consumes = "application/json" )
-    String post( @RequestBody @Valid Invoice invoice )
+    Invoice post( @RequestBody @Valid Invoice invoice )
     {
-        return invoice.getLines().stream()
-                .map( Line::toString )
-                .collect( Collectors.joining( "\n" ) );
+        return invoice;
     }
 }
--- server/src/test/java/com/liberologico/invoice_api/InvoiceApiApplicationTests.java
+++ server/src/test/java/com/liberologico/invoice_api/InvoiceApiApplicationTests.java
@@ -1,18 +1,73 @@
 package com.liberologico.invoice_api;
 
+import com.google.gson.Gson;
+import com.liberologico.invoice_api.entities.Invoice;
+import com.liberologico.invoice_api.entities.Line;
+import com.liberologico.invoice_api.entities.Person;
+import com.liberologico.invoice_api.entities.Price;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import retrofit.Call;
+import retrofit.Response;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 @RunWith( SpringJUnit4ClassRunner.class )
 @SpringApplicationConfiguration( classes = InvoiceApiApplication.class )
 @WebAppConfiguration
 public class InvoiceApiApplicationTests
 {
+    private static final String ROOT = "http://localhost:8080";
+
+    private final InvoiceService service = new InvoiceClient( ROOT ).getService();
+
     @Test
-    public void contextLoads()
+    public void emptyBody() throws IOException
     {
+        Date now = new Date();
+
+        Call<Invoice> call = service.post( new Invoice() );
+
+        Response<Invoice> response = call.execute();
+        assertFalse( response.isSuccess() );
+        assertEquals( 400, response.code() );
+        assertNotNull( response.errorBody() );
+
+        Gson gson = InvoiceClient.getGsonBuilder().create();
+        ApiError errorBody = gson.fromJson( response.errorBody().string(), ApiError.class );
+        assertNotNull( errorBody );
+        assertTrue( now.before( errorBody.timestamp ) );
+    }
+
+    @Test
+    public void simpleInvoice() throws IOException
+    {
+        Invoice invoice = new Invoice()
+                .setHolder( new Person().setEmail( "bu@del.lo" ) )
+                .setRecipient( new Person().setEmail( "di@tu.ma" ) )
+                .setLines( Arrays.asList(
+                    new Line().setDescription( "Riga 1" )
+                              .setPrice( new Price().setPrice( BigDecimal.TEN ).setCurrency( "EUR" ) ),
+                    new Line().setDescription( "Riga 2" )
+                              .setPrice( new Price().setPrice( BigDecimal.ONE ).setCurrency( "EUR" ) )
+                ) );
+
+        Call<Invoice> call = service.post( invoice );
+
+        Response<Invoice> response = call.execute();
+        assertTrue( response.isSuccess() );
+        assertEquals( 200, response.code() );
+        assertNotNull( response.body() );
     }
 }
