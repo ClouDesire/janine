9c0513878cab2a06160c67692e1810717ab67f9d
Matteo Giordano <matteo.giordano@cloudesire.com>
2015-12-21 18:23:50 +0100

    Pump up the timeouts

--- client/src/main/java/com/liberologico/janine/InvoiceClient.java
+++ client/src/main/java/com/liberologico/janine/InvoiceClient.java
@@ -1,10 +1,13 @@
 package com.liberologico.janine;
 
 import com.google.gson.GsonBuilder;
+import com.squareup.okhttp.OkHttpClient;
 import io.gsonfire.DateSerializationPolicy;
 import io.gsonfire.GsonFireBuilder;
 import retrofit.GsonConverterFactory;
 import retrofit.Retrofit;
+
+import java.util.concurrent.TimeUnit;
 
 public class InvoiceClient
 {
@@ -12,9 +15,14 @@ public class InvoiceClient
 
     public InvoiceClient( String baseUrl )
     {
+        OkHttpClient client = new OkHttpClient();
+        client.setConnectTimeout( 30, TimeUnit.SECONDS ); // connect timeout
+        client.setReadTimeout( 60, TimeUnit.SECONDS );    // socket timeout
+
         retrofit = new Retrofit.Builder()
                 .baseUrl( baseUrl )
                 .addConverterFactory( GsonConverterFactory.create( getGsonBuilder().create() ) )
+                .client( client )
                 .build();
     }
 
