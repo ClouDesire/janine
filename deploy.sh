9cd5afe9327aad0ff629c62e583ce2bf621d2e1e
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2015-12-28 12:05:59 +0100

    travis: fix bad if in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
 mvn deploy -Dmaven.test.skip=true -Dgpg.keyname=$CI_DEPLOY_GPG_KEYID -Dgpg.skip=false -Dgpg.passphrase=$CI_DEPLOY_GPG_SECRET -Dgpg.publicKeyring=../.travis/public.gpg -Dgpg.secretKeyring=../.travis/private.gpg --settings ../.travis/settings.xml
-if [ -x dockerbuild.sh ]
+if [ -x dockerbuild.sh ]; then
   ./dockerbuild.sh
 fi
