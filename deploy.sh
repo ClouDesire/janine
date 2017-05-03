02635a8728914e06ebe4dad04a2685a0a1887358
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2017-05-03 16:05:57 +0200

    May fix master build

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
-mvn deploy -Dmaven.test.skip=true -Dgpg.keyname=$CI_DEPLOY_GPG_KEYID -Dgpg.skip=false -Dgpg.passphrase=$CI_DEPLOY_GPG_SECRET -Dgpg.publicKeyring=../.travis/public.gpg -Dgpg.secretKeyring=../.travis/private.gpg --settings ../.travis/settings.xml
-if [ -x dockerbuild.sh ]; then
-  ./dockerbuild.sh
+echo Running maven deploy
+./mvnw -B deploy -Dmaven.test.skip=true -Dgpg.keyname=$CI_DEPLOY_GPG_KEYID -Dgpg.skip=false -Dgpg.passphrase=$CI_DEPLOY_GPG_SECRET -Dgpg.publicKeyring=../.travis/public.gpg -Dgpg.secretKeyring=../.travis/private.gpg --settings ../.travis/settings.xml
+if [ -x server/dockerbuild.sh ]; then
+  echo Running dockerbuild
+  ./server/dockerbuild.sh
 fi
