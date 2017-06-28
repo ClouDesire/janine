19fc2d9def4ffdbdf11a26a6377f022341af6195
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2017-06-28 15:32:53 +0200

    Switch to Bintray (#42)

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
 #!/bin/bash -e
-echo $PRIVATE_GPG | base64 -d > ./.gpg/private.gpg
-chmod 600 ./.gpg/private.gpg
-echo Running maven deploy
-./mvnw -B deploy -Dmaven.test.skip=true -Dgpg.skip=false -Dgpg.publicKeyring=./.gpg/public.gpg -Dgpg.secretKeyring=./.gpg/private.gpg
-echo Running dockerbuild
+echo Deploy server container
 cd ./janine-server && ./dockerbuild.sh
